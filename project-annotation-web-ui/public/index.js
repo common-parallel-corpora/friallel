27cfa20bf7b088b2adaed492c9975dfe0aa57bbc
Moussa Doumbouya <doumbouya.moussa@gmail.com>
2022-09-07 12:07:44 -0700

    App module for web app in index.js; Report connection status on UI

---
+++ project-annotation-web-ui/public/index.js
@@ -0,0 +1,56 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-analytics.js";
+import { doc, getDoc, getFirestore } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBQja_MCcubMhjmJYhKI50H_Nzn8SkUwgY",
+  authDomain: "fs-2022-003-mtannotation.firebaseapp.com",
+  databaseURL:
+    "https://fs-2022-003-mtannotation-default-rtdb.firebaseio.com",
+  projectId: "fs-2022-003-mtannotation",
+  storageBucket: "fs-2022-003-mtannotation.appspot.com",
+  messagingSenderId: "82914623747",
+  appId: "1:82914623747:web:80acd4fb0d31df013ca936",
+  measurementId: "G-LXY5Y1HL47",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+
+const docRef = doc(firestore, "dataset-flores-dev", "0000000000");
+const docSnap = await getDoc(docRef);
+const doc_data = docSnap.data();
+console.log(doc_data)
+
+// TODO: change this to tranSlations
+doc_data.tranlations.forEach(translation => {
+  const selector = "#" + `txt-${translation.lang}`;
+  console.log("Selector", selector);
+  console.log("Value", translation.translation);
+  $(selector).text(
+    translation.translation
+  );
+});
+
+
+const database = getDatabase();
+const connectedRef = ref(database, ".info/connected");
+onValue(connectedRef, (snap) => {
+  if (snap.val() === true) {
+    $("#connection-state").removeClass("synchro-light-offline");
+    $("#connection-state").addClass("synchro-light-online");
+    
+  } else {
+    $("#connection-state").removeClass("synchro-light-online");
+    $("#connection-state").addClass("synchro-light-offline");
+  }
+});
