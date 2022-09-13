6fa6e75267ac8804f12fd15966106fb7df484cb1
Abdoulaye Sow <abdoulaye.sow.1989@gmail.com>
2022-09-12 23:12:21 -0500

    Mise en place de la function d'assignment de taches

--- project-annotation-web-ui/functions/index.js
+++ project-annotation-web-ui/functions/index.js
@@ -3,10 +3,12 @@ const admin = require("firebase-admin");
 const {getFirestore} = require("firebase-admin/firestore");
 
 
+
 admin.initializeApp();
 const db = getFirestore();
 
 const translationTaskCollectionName = "tanslation-tasks";
+const userCollectionName = "users";
 
 // Retrieving Data in Configuration File .env
 const maxAssignmentHours = process.env.MAX_TASK_ASSIGNMENT_AGE_HOURS;
@@ -62,11 +64,95 @@ function unassignExpiredAssignments() {
   });
 }
 
+/* Get the user workload size */
+// eslint-disable-next-line no-unused-vars
+function getUserWorkloadSize(userID){
+  var workLoad = 0;
+  db.collection(
+    translationTaskCollectionName
+  ).where(
+    "status", "==", "assigned"
+  ).where(
+      "assignee_id", "==", userID
+  ).get().then((assignedTasks)=>{
+    // console.log(userID, " has these tasks assigned : ", assignedTasks.size);
+    workLoad = assignedTasks.size;
+    console.log(userID, " has these tasks assigned : ", workLoad);
+  });
+  return workLoad;
+}
+
+function getUserTranslatedSentences(userID){
+    console.log(userID);
+    return ["0000000002"];
+}
+
+function assignTaskToUser(userID, workloadLoad){
+
+  // const userTranslatedSentences = user.getData().translatedSentences;
+  const userTranslatedSentences = getUserTranslatedSentences(userID);
+
+  db.collection(translationTaskCollectionName).where(
+  "status", "==", "unassigned"
+  ).where(
+    "document_id", "not-in", userTranslatedSentences
+  ).orderBy(
+    "document_id"
+  ).orderBy(
+    "priority", "asc"
+  ).limit(workloadLoad)
+  .get().then((availableTasks)=>{
+    availableTasks.docs.forEach((availableTask)=>{
+      availableTask.ref.set({
+        "status": "assigned",
+        "assignee_id": userID,
+        "assigned_date": new Date(),
+      }, {merge: true}).then(()=>{
+        console.log("Assigned Task #", availableTask.id, " to User : ", userID);
+      });
+    });
+  });
+}
+
+/** Assign Task to increase user workloads to their max */
+function updateUserWorkload() {
+  // Retrieve All actives users with buckets not full
+  db.collection(userCollectionName).where(
+  "isActiveTranslator", "==", true
+  ).get().then((activeUsers)=>{
+    // For each user, fill the bucket by getting the next available tasks
+    activeUsers.docs.forEach((activeUser)=>{
+      // Check workload of Active user
+      // var userWorkLoadSize = getUserWorkloadSize(activeUser.id);
+
+      var userWorkLoadSize = 0;
+      db.collection(translationTaskCollectionName).where(
+      "status", "==", "assigned"
+      ).where(
+          "assignee_id", "==", activeUser.id
+      ).get().then((assignedTasks)=>{
+        // console.log(userID, " has these tasks assigned : ", assignedTasks.size);
+        userWorkLoadSize = assignedTasks.size;
+        console.log(activeUser.id, " has the current load size : ", userWorkLoadSize);
+        if(userWorkLoadSize < userBucketMaxSize){
+          console.log("UserWorkLoad ", userWorkLoadSize, " is under the max :", userBucketMaxSize);
+          const workLoadToAssign = userBucketMaxSize - userWorkLoadSize;
+          assignTaskToUser(activeUser.id, workLoadToAssign);
+        }
+      });
+    });
+  });
+}
+
 exports.taskAssignmentAgent = functions.pubsub.schedule("every 15 minutes").onRun((context) => {
-  console.log("Task Assignment Max Age in HOURS : ", maxAssignmentHours);
-  console.log("User Bucket Max Size : ", userBucketMaxSize);
+  console.log("ENV VARIABLE : MAX AGE TASK in HOURS : ", maxAssignmentHours);
+  console.log("ENV VARIABLE : USER BUCKET SIZE MAX : ", userBucketMaxSize);
   console.log(context);
 
+  // Step 1: Unassign expired tasks
   unassignExpiredAssignments();
+
+  // Step 2: Assign Tasks to fill user buckets
+  updateUserWorkload();
   return null;
 });
