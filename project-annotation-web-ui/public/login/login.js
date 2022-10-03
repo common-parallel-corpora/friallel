9beae26fc36907d9329fc5c7a8dca2f85598491a
DOUMBOUYA Séré Moussa <dseremoussa@gmail.com>
2022-10-03 10:39:01 +0200

    modification de la page de login Ajout des pages policy et privacy Correction d'orthographe sur les noeuds firebase

--- project-annotation-web-ui/public/login/login.js
+++ project-annotation-web-ui/public/login/login.js
@@ -25,16 +25,14 @@ var uiConfig = {
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     ],
     // tosUrl and privacyPolicyUrl accept either url string or a callback
     // function.
     // Terms of service url/callback.
-    tosUrl: "facebook.com",
+    tosUrl: "privacy.html",
     // Privacy policy url/callback.
     privacyPolicyUrl: function() {
-      window.location.assign("youtube.com");
+      window.location.assign("policy.html");
     }
 };
   
