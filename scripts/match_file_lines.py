a9045717a3d37d7a97aae375789b75093963488d
Moussa Doumbouya <doumbouya.moussa@gmail.com>
2023-02-19 10:39:30 -0800

    use edit distance when building distance matrix for alignment

--- scripts/match_file_lines.py
+++ scripts/match_file_lines.py
@@ -9,6 +9,8 @@ from sklearn.metrics.pairwise import cosine_distances
 from tqdm import tqdm
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
+import editdistance
+
 
 
 def parse_args():
@@ -40,16 +42,29 @@ def lines_to_vectors(lines):
     return np.vstack(vectors)
 
 
+def build_edit_distance_matrix(lines_1, lines_2):
+    n1 = len(lines_1)
+    n2 = len(lines_2)
+    distance_matrix = np.zeros((n1, n2))
+    for i in tqdm(range(n1)):
+        for j in range(i, n2):
+            dist = editdistance.eval(lines_1[i], lines_2[j])
+            distance_matrix[i, j] = dist
+            distance_matrix[j, i] = dist
+    return distance_matrix
+
+
 def main(args):
     p1, p2 = (Path(p) for p in args.input_files)
     lines_1 = read_file_lines(p1)
     lines_2 = read_file_lines(p2)
-    print("computing vectors")
-    vectors_1 = lines_to_vectors(lines_1)
-    vectors_2 = lines_to_vectors(lines_2)
+    # print("computing vectors")
+    #vectors_1 = lines_to_vectors(lines_1)
+    #vectors_2 = lines_to_vectors(lines_2)
     print("computing distance matrix")
     
-    distance_matrix = cosine_distances(vectors_1, vectors_2)
+    # distance_matrix = cosine_distances(vectors_1, vectors_2)
+    distance_matrix = build_edit_distance_matrix(lines_1, lines_2)
     # distance_matrix = scipy.spatial.distance_matrix(vectors_1, vectors_2)
     print("computing assignment")
     ind1, ind2 = scipy.optimize.linear_sum_assignment(distance_matrix)
